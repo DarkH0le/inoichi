f0a1f50ee1baacb8e0ba1427e2fdcc6738b32102
DarkH0le <andresivan.aldama@gmail.com>
2019-03-15 02:11:23 -0600

    Firsebase working

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCw32g6WdOwTFSGt7c3FYF2HutwYkAgp8k",
+    authDomain: "inoichi-97330.firebaseapp.com",
+    databaseURL: "https://inoichi-97330.firebaseio.com",
+    projectId: "inoichi-97330",
+    storageBucket: "inoichi-97330.appspot.com",
+    messagingSenderId: "851575516206"
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const provider = new firebase.auth.GoogleAuthProvider();
+
+export default firebase;
